9cdb751f7446f57ec62ee9ac3f03388145fd9956
valesquiveel <139177805+valesquiveel@users.noreply.github.com>
2023-11-21 11:49:37 -0300

    Update script.js

--- Aula 01 - introdução/Atividades/01/js/script.js
+++ Aula 01 - introdução/Atividades/01/js/script.js
@@ -5,4 +5,15 @@ var valorTotalConta = Number(prompt("Digite o valor total da conta:"));
 
 var totalCadaCliente = valorTotalConta / numeroClientes;
 
-alert(`o valor da conta para cada pessoa na mesa é de: R$${totalCadaCliente}`);
+alert(`o valor da conta para cada pessoa na mesa é de: R$${totalCadaCliente}`); 
+
+// function calculeConta(numeroClientes, valorTotalConta){
+// alert("Calcule a sua conta:");
+
+// let numeroClientes = Number(prompt("Digite o número de pessoas na mesa:"));
+// let valorTotalConta = Number(prompt("Digite o valor total da conta:"));
+
+// let totalCadaCliente = valorTotalConta / numeroClientes;
+
+// alert(`o valor da conta para cada pessoa na mesa é de: R$${totalCadaCliente}`);
+// }
