2b9d6b9673941f69d55b5bff254c268b4eefb7b8
valesquiveel <139177805+valesquiveel@users.noreply.github.com>
2023-11-24 08:11:27 -0300

    Update script.js

--- Aula 03 - repetições e array/Atividades/03/js/script.js
+++ Aula 03 - repetições e array/Atividades/03/js/script.js
@@ -1,10 +1,14 @@
-var contador = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"] 
+var contador = 0
 var multi = 1
 
 while (multi <= 10) {
-    console.log(`${contador[0]} x ${multi} = ${contador[0] * multi++}`)
+    while (contador <= 10) {
+        console.log(`${multi} x ${contador} = ${multi * contador}`)
+        contador++
+    }
+    var contador = 0
+    multi++
 }
-
 
 
 
