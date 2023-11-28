4c227828fd691a18be70c3c866e2c04fd841d02d
valesquiveel <139177805+valesquiveel@users.noreply.github.com>
2023-11-28 11:54:15 -0300

    Atividade Revisão

---
+++ Aula 05 - Objetojs/Atividades/Atividade Revisão/js/script.js
@@ -0,0 +1,32 @@
+class Filmes {
+    nome;
+    descricao;
+    data;
+    diretor;
+    categoria;
+}
+
+var btnFilmes = document.querySelector("#btnFilmes")
+btnFilmes.addEventListener("click", addFilme)
+
+function addFilme() {
+    let nomeInp = document.querySelector("#nome").value
+    let descricaoInp = document.querySelector("#descricao").value
+    let dataInp = document.querySelector("#data").value
+    let diretorInp = document.querySelector("#diretor").value
+    let categoriaInp = document.querySelector("#categoria").value
+    let resposta = document.querySelector("#resposta")
+
+    var filme = new Filmes();
+    filme.nome = nomeInp
+    filme.descricao = descricaoInp
+    filme.data = dataInp
+    filme.diretor = diretorInp
+    filme.categoria = categoriaInp
+
+    resposta.innerHTML = `<div class="add"><h3>${filme.nome}</h3>
+    <p>Descrição: ${filme.descricao}</p>
+    <p>Data: ${filme.data}</p>
+    <p>Diretor: ${filme.diretor}</p>
+    <p>Categoria: ${filme.categoria}</p></div>`
+}
